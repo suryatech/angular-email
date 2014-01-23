680e5dce44e3b295aa768989cd5cf97f1fa6494f
SURYA PAPPOPPULA <suryasongs@hmail.com>
2014-01-23 23:44:26 +0530

    Day 1 Progress

--- js/main.js
+++ js/main.js
@@ -1,8 +1,63 @@
-var app = angular.module('myFirstApp', []);
+var app = angular.module('myFirstApp', ['ngRoute']);
 app.config(function($routeProvider) {
 	$routeProvider.when('/', {
-		template: 'templates/home.html',
+		templateUrl: 'templates/home.html',
 		controller: 'HomeController'
 	})
-	.otherwise({redirectTo: '/'});
+		.when('/settings', {
+			templateUrl: 'templates/settings.html',
+			controller: 'SettingsController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+
+app.controller('HomeController', function($scope) {
+	$scope.selectedMail;
+
+	$scope.setSelectedMail = function(mail){
+		$scope.selectedMail = mail;
+	}
+
+	$scope.isSelected = function(mail){
+		if($scope.selectedMail){
+			return $scope.selectedMail === mail
+		}
+	}
+});
+
+app.controller('SettingsController', function($scope) {
+	// used to populate it will default values of the user
+
+	$scope.settings = {
+		name: "Surya",
+		email: "me@example.com",
+	}
+
+	$scope.updateSettings = function() {
+		console.log('UpdateSettings called');
+	}
+});
+
+app.controller('mailListingController', ['$scope','$http', function($scope, $http) {
+	$scope.email = [];
+
+	$http({
+		method: 'GET',
+		url: 'api/data.php'
+	})
+	.success(function(data, status, headers){
+    	$scope.email = 	data.all;
+   	})
+	.error(function(data, status, headers){
+
+	});
+
+}]);
+
+
+
+app.controller('contentController', function($scope) {
+
 });
